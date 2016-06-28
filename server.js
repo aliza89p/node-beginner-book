354b35158fde475647ef977d2a62b7d819baf741
Aliza Pilisuk <apilisuk@gmail.com>
2016-06-28 16:02:26 -0700

    created start function to export, required server file in index.js and called the start function

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http = require('http');
 
-function onRequest(reqest, response) {
-  console.log('Request received');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
+function start() {
+  function onRequest(reqest, response) {
+    console.log('Request received');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
 }
-http.createServer(onRequest).listen(8888);
 
-console.log('Server has started.');
+exports.start = start;
